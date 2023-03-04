d5c8cecf1c99052f29b999b601527464076378b4
Tyler McRill <tjmcrill@gmail.com>
2023-03-04 17:59:11 -0500

    basic embedding pr

--- src/pages/api/summarize.ts
+++ src/pages/api/summarize.ts
@@ -1,3 +1,4 @@
+import transcript from "mock/transcript";
 import { NextApiRequest, NextApiResponse } from "next";
 import AwsService from "../../services/aws-service";
 import OpenAIService from "../../services/openai-service";
@@ -12,15 +13,16 @@ interface Request extends NextApiRequest {
 }
 
 const summarize = async (req: Request, res: NextApiResponse<Data>) => {
+  /*
   const awsTranscriptResponse = await AwsService.generateTranscript(
     req.body.searchTerm,
   );
-  console.log(awsTranscriptResponse)
+  
   const openAiSummaryResponse = await OpenAIService.summarizeVideos(
     awsTranscriptResponse,
   );
-
-  res.status(200).json({ summaries: openAiSummaryResponse });
+  */
+  res.status(200).json({ summaries: transcript as unknown as VideoSummary[] });
 };
 
 export default summarize;
