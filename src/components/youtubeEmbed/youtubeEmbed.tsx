f9a69a7658f15be5432f1e46cde19052d77db223
ian <iankush1317@gmail.com>
2023-03-04 16:42:30 -0500

    trying to fix iframe flash

--- src/components/youtubeEmbed/youtubeEmbed.tsx
+++ src/components/youtubeEmbed/youtubeEmbed.tsx
@@ -1,23 +1,22 @@
-import { useRef } from "react";
+import { useState } from "react";
 
 const youtubeEmbed = ({ embedId }: { embedId: string }) => {
-  const iframeRef = useRef<HTMLIFrameElement>(null);
+  const [loaded, setLoaded] = useState(false);
+
+  const handleLoad = () => {
+    setLoaded(true);
+  };
 
   return (
     <div className="video-responsive">
       <iframe
-        ref={iframeRef}
         src={`https://www.youtube.com/embed/${embedId}`}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
         title="Embedded youtube"
         className="aspect-video w-full"
-        style={{ visibility: "hidden" }}
-        onLoad={() => {
-          if (iframeRef.current) {
-            iframeRef.current.style.visibility = "visible";
-          }
-        }}
+        style={{ visibility: loaded ? "visible" : "hidden" }}
+        onLoad={handleLoad}
       />
     </div>
   );
