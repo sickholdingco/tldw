67a8948661055d90ca2d4929a548d6016c49fe94
ian <iankush1317@gmail.com>
2023-03-04 20:44:25 -0500

    hooks

--- src/server/api/routers/embedding.ts
+++ src/server/api/routers/embedding.ts
@@ -2,16 +2,25 @@ import { z } from "zod";
 import EmbeddingService from "@/server/services/openai/embedding-service";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { TRPCError } from "@trpc/server";
 
 export const embeddingRouter = createTRPCRouter({
   embedding: publicProcedure
-    .input(z.object({ searchTerm: z.string() }))
+    .input(z.object({ question: z.string() }))
     .query(async ({ input }) => {
-      const embeddingResponse = await EmbeddingService.generateEmbedding(
-        "big poopie",
-      );
-      return {
-        name: embeddingResponse,
-      };
+      try {
+        const embeddingResponse = await EmbeddingService.generateEmbedding(
+          "big poopie",
+        );
+        return {
+          data: embeddingResponse,
+          status: "SUCCESS",
+        };
+      } catch (err) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to fetch embedding",
+        });
+      }
     }),
 });
--- src/server/api/routers/summarize.ts
+++ src/server/api/routers/summarize.ts
@@ -2,13 +2,22 @@ import { z } from "zod";
 import summaries from "mock/summaries";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { TRPCError } from "@trpc/server";
 
 export const summarizeRouter = createTRPCRouter({
   summarize: publicProcedure
     .input(z.object({ searchTerm: z.string() }))
     .query(async ({ input }) => {
-      return {
-        summaries: summaries,
-      };
+      try {
+        return {
+          data: summaries,
+          status: "SUCCESS",
+        };
+      } catch (err) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to fetch summary",
+        });
+      }
     }),
 });
