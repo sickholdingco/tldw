e65d5c60a83e86478093419a824de3ad195434a4
Tyler McRill <tjmcrill@gmail.com>
2023-03-21 19:42:48 -0400

    cum

--- tldw-aws-api/functions/youtube_transcript.py
+++ tldw-aws-api/functions/youtube_transcript.py
@@ -7,8 +7,6 @@ from googleapiclient.discovery import build
 
 def generateTranscript(event, context):
 	client = boto3.client('lambda')
-	dynamodb = boto3.resource('dynamodb')
-	table = dynamodb.Table('topic_info')
 
 	api_key = os.environ["YOUTUBE_API_KEY"]
 	youtube = build('youtube','v3',developerKey = api_key)
@@ -28,40 +26,39 @@ def generateTranscript(event, context):
 			'title': item['snippet']['title'], 
 			'thumbnail': item['snippet']['thumbnails']['high']['url']
 		}) 
-	num_blocks = 0
-	block_id = 0
-	for vid in vid_info:
-		try:
-			dict_list = YouTubeTranscriptApi.get_transcript(vid['id'])
-			result = ""
-			for d in dict_list:
-				result += " " + d['text']
 
-			n = math.ceil((len(result)/4) / 3600)
-			num_blocks += n
-			blocks = []
-			part_length = len(result) // n
-			for i in range(n):
-				blocks.append({
-					'blockId': block_id,
-					'text': result[i * part_length : (i + 1) * part_length]
-				})
-				block_id = block_id + 1
+		num_blocks = 0
+		block_id = 0
+		for vid in vid_info:
+			try:
+				dict_list = YouTubeTranscriptApi.get_transcript(vid['id'])
+				result = ""
+				for d in dict_list:
+					result += " " + d['text']
 
-			vid['blocks'] = blocks
-		except:
-			vid['blocks'] = []
-			print('this video does not have transcription!')
+				n = math.ceil((len(result)/4) / 3600)
+				num_blocks += n
+				blocks = []
+				part_length = len(result) // n
+				for i in range(n):
+					blocks.append({
+						'blockId': block_id,
+						'text': result[i * part_length : (i + 1) * part_length]
+					})
+					block_id = block_id + 1
+
+				vid['blocks'] = blocks
+			except:
+				vid['blocks'] = []
+				print('this video does not have transcription!')
+
 	payload = {
-	    'key1': 'value1'
+		"vid_info": vid_info,
+		"num_blocks": num_blocks
 	}
 	response = client.invoke(
-	    FunctionName='embeddings',
-	    Payload=json.dumps(payload)
+			FunctionName='embeddings',
+			Payload=json.dumps(payload)
 	)
-  # unused, can we remove this @tyler?
-	response_payload = json.loads(response['Payload'].read().decode('utf-8'))
-
-	response = table.put_item(Item=vid_info)
 
 	return vid_info
