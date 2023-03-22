440d36a076b031166fb3dd78fcec37c1b1f79140
hamy <jshamilton1414@gmail.com>
2023-03-22 00:19:10 -0400

    added dynamodb support

--- tldw-aws-api/youtube_transcript_module/index.py
+++ tldw-aws-api/youtube_transcript_module/index.py
@@ -1,6 +1,7 @@
 import os
 import math
 import json
+from uuid import uuid4
 import boto3
 from youtube_transcript_api import YouTubeTranscriptApi
 from googleapiclient.discovery import build
@@ -8,28 +9,33 @@ from googleapiclient.discovery import build
 def generateTranscript(event, context):
 	client = boto3.client('lambda')
 
+	TOPIC_INFO_TABLE = os.environ['TOPIC_INFO_TABLE']
+	dbClient = boto3.client('dynamodb')
+
 	api_key = os.environ["YOUTUBE_API_KEY"]
 	youtube = build('youtube','v3',developerKey = api_key)
 	prompt = event['searchTerm']
+	search_filters = {'videoCaption': 'closedCaption'}
 	request = youtube.search().list(
 		part="snippet",
 		maxResults=3,
 		type="video",
+		videoCaption=search_filters['videoCaption'],
 		q=prompt
 	)	
 	response = request.execute()
 	items = response['items']
-	vid_info = []
+	search_videos = []
 	for item in items:
-		vid_info.append({ 
+		search_videos.append({ 
 			'id':  item['id']['videoId'],
 			'title': item['snippet']['title'], 
 			'thumbnail': item['snippet']['thumbnails']['high']['url']
-		}) 
+		})
 
 		num_blocks = 0
 		block_id = 0
-		for vid in vid_info:
+		for vid in search_videos:
 			try:
 				dict_list = YouTubeTranscriptApi.get_transcript(vid['id'])
 				result = ""
@@ -52,13 +58,25 @@ def generateTranscript(event, context):
 				vid['blocks'] = []
 				print('this video does not have transcription!')
 
-	payload = {
-		"vid_info": vid_info,
-		"num_blocks": num_blocks
-	}
-	response = client.invoke(
-			FunctionName='embeddings',
-			Payload=json.dumps(payload)
+	dbClient.put_item(
+		TableName=TOPIC_INFO_TABLE,
+		Item={
+				'id': {
+					'S': str(uuid4())
+				},
+				'search_videos': {
+					'S': json.dumps(search_videos)
+				}
+		}
 	)
 
-	return vid_info
+	payload = {
+		"search_videos": search_videos,
+		"num_blocks": num_blocks
+	}
+	# response = client.invoke(
+	# 		FunctionName='embeddings',
+	# 		Payload=json.dumps(payload)
+	# )
+
+	return search_videos
