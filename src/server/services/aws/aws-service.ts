e2ed107e3fca90aad3da50da0ff164b43b671bd2
jhamilton14 <jshamilton1414@gmail.com>
2023-04-15 22:53:55 -0400

    added answer question

--- src/server/services/aws/aws-service.ts
+++ src/server/services/aws/aws-service.ts
@@ -13,13 +13,25 @@ const lambda = new AWS.Lambda();
 
 const generateTranscript = async (searchTerm: string): Promise<VideoType[]> => {
   const params = {
-    FunctionName: "youtube-transcript",
+    FunctionName: "tldw-aws-api-dev-youtube-transcript",
     Payload: JSON.stringify({ searchTerm }),
   };
+
   const result = await lambda.invoke(params).promise();
   const transcript = JSON.parse(result.Payload as string);
   return transcript;
 };
 
-const AwsService = { generateTranscript };
+const answerQuestion = async (question: string): Promise<string> => {
+  const params = {
+    FunctionName: "tldw-node-api-dev-pineconeQuery",
+    Payload: JSON.stringify({ question }),
+  };
+
+  const result = await lambda.invoke(params).promise();
+  const answer = JSON.parse(result.Payload as string);
+  return answer;
+};
+
+const AwsService = { generateTranscript, answerQuestion };
 export default AwsService;
