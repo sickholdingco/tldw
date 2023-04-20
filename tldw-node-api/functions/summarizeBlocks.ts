dc40b7a613aaba8b081d349fdbe62b6d95809c42
Tyler McRill <tjmcrill@gmail.com>
2023-04-19 23:40:39 -0400

    consume both hooks

--- tldw-node-api/functions/summarizeBlocks.ts
+++ tldw-node-api/functions/summarizeBlocks.ts
@@ -16,19 +16,25 @@ interface Event {
 export const generate = async (event: Event) => {
   const jsonStringBlocks = JSON.stringify(event.blocks);
   const parsedBlocks = JSON.parse(jsonStringBlocks);
-  const summaries: Summary[] = await Promise.all(
-    parsedBlocks.map(async (block: Block) => {
-      const summaryResponse = await summarize(block.text);
-      return {
-        blockId: block.blockId,
-        summary: summaryResponse,
-      };
-    }),
-  );
+  try {
+    const summaries: Summary[] = await Promise.all(
+      parsedBlocks.map(async (block: Block) => {
+        const summaryResponse = await summarize(block.text);
+        return {
+          blockId: block.blockId,
+          summary: summaryResponse,
+        };
+      }),
+    );
 
-  console.log(summaries);
-  return {
-    statusCode: 200,
-    body: summaries,
-  };
+    return {
+      statusCode: 200,
+      body: summaries,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: error,
+    };
+  }
 };
