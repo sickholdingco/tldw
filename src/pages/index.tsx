9bd70862b81bba953f991dc950a2427baaaf13db
jhamilton14 <jshamilton1414@gmail.com>
2023-04-27 22:20:55 -0400

    changed favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>tl;dw</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔍</text></svg>" />
       </Head>
 
       <main className="m-auto flex w-full max-w-[90rem] flex-1 flex-col px-8 py-6">
